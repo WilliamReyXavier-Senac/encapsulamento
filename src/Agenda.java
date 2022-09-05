895f6d383394935231baa1e7932adccfe03900ad
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-09-05 19:57:03 -0300

    Adicionado classe agenda e main

---
+++ src/Agenda.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author willian.xavier
+ */
+public class Agenda {
+    private int dia;
+    private int mes;
+    private String anotacao;
+    
+    void anotar(int d, int m,  String nota) {
+        dia = d;
+        mes = m;
+        anotacao = nota;
+        validaData();
+    }
+    private void validaData() {
+        if((dia < 1) || (dia > 31) || (mes > 12) || (mes < 1)) {
+            dia = 0;
+            mes = 0;
+            anotacao = "Anotação inválida";
+        }
+    }
+    
+    void mostraAnototacao() {
+        System.out.println(dia + "/" + mes + " : " + anotacao);
+    }
+    
+    
+}
